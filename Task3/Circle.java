69d02417ced76d8bfcf7114d4beb3c7aabdc14d4
SimranChaudhary2007 <simranchaudhary434@gmail.com>
2025-05-31 14:27:51 +0545

    Complete OOP Task

---
+++ Task3/Circle.java
@@ -0,0 +1,40 @@
+/* Write a Java program to create a class called "Circle" with a radius attribute. You can access 
+this attribute using setter and getter method. Calculate the area and circumference of the circle.*/
+
+package Task3;
+
+public class Circle {
+    private double  radius;
+
+
+    // setter
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    // getter
+    public double getRadius(){
+        return radius;
+    }
+
+    // method to claculate area
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+
+    // method to calculate circumference
+    public double getCircumference(){
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Circleimp{
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.setRadius(5);
+
+        System.out.println("Radius: " + c.getRadius());
+        System.out.println("Area: " + c.getArea());
+        System.out.println("Circumference: " + c.getCircumference());
+    }
+}
